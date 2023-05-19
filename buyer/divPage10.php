0d9271fecb53a3338347dfb01721e9eabe294908
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-19 08:57:03 +0100

    Update divPage10.php

--- buyer/divPage10.php
+++ buyer/divPage10.php
@@ -10,158 +10,108 @@ if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
 }
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>
+<script type="text/javascript">
+             function ajaxinfo() {
+                $.ajax({
+                    type: 'GET',
+                    url: 'ajaxinfo.html',
+                    timeout: 10000,
 
-    <ul class="nav nav-tabs">
-        <li class="active"><a href="#filter" data-toggle="tab">Filter</a></li>
-    </ul>
-    <div id="myTabContent" class="tab-content">
-        <div class="tab-pane active in" id="filter">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Tutorial Name</th>
-                        <th>Description</th>
-                        <th>Seller</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-  <td>
-                            <input class='filterinput form-control input-sm' name="tuto_name" size='3'>
-                        </td>
-                        <td>
-                            <input class='filterinput form-control input-sm' name="tuto_info" size='3'>
-                        </td>
-                        <td>
-                            <select class='filterselect form-control input-sm' name="tuto_seller">
-                                <option value="">ALL</option>
-                                <?php
-$query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `tutorials` WHERE `sold` = '0' or `sold` = '1' ORDER BY resseller ASC");
-	while($row = mysqli_fetch_assoc($query)){
-		 $qer = mysqli_query($dbcon, "SELECT DISTINCT(`id`) FROM resseller WHERE username='".$row['resseller']."' ORDER BY id ASC")or die(mysql_error());
-		   while($rpw = mysqli_fetch_assoc($qer))
-			 $SellerNick = "seller".$rpw["id"]."";
-	echo '<option value="'.$SellerNick.'">'.$SellerNick.'</option>';
-	}
-?>
-                            </select>
-                        </td>
-                        <td>
-                            <button id='filterbutton' class="btn btn-primary btn-sm" disabled>Filter <span class="glyphicon glyphicon-filter"></span></button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-
-    <table width="100%" class="table table-striped table-bordered table-condensed sticky-header" id="table">
-        <thead>
-            <tr>
-                <th scope="col">Tutorial Name</th>
-                <th scope="col">Description</th>
-                <th scope="col">Seller</th>
-                <th scope="col">Price</th>
-                <th scope="col">Added on </th>
-                <th scope="col">Buy</th>
-            </tr>
-        </thead>
-        <tbody>
-
-            <?php
-$q = mysqli_query($dbcon, "SELECT * FROM tutorials WHERE sold='0' or sold='1' ORDER BY RAND()")or die(mysqli_error());
- while($row = mysqli_fetch_assoc($q)){
-	    $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
-		   while($rpw = mysqli_fetch_assoc($qer))
-			 $SellerNick = "seller".$rpw["id"]."";
-     echo "
- <tr>     
-    <td id='tuto_name'> ".htmlspecialchars($row['tutoname'])." </td> 
-	<td id='tuto_info'> ".htmlspecialchars($row['infos'])." </td>
-    <td id='tuto_seller'> ".htmlspecialchars($SellerNick)."</td>
-    <td> ".htmlspecialchars($row['price'])."</td>
-	    <td> ".$row['date']."</td>";
-    echo '
-    <td>
-	<span id="tuto'.$row['id'].'" title="buy" type="tuto"><a onclick="javascript:buythistool('.$row['id'].')" class="btn btn-primary btn-xs"><font color=white>Buy</font></a></span><center>
-    </td>
-            </tr>
-     ';
- }
-
- ?>
-                <script type="text/javascript">
-                    $('#filterbutton').click(function() {
-                        $("#table tbody tr").each(function() {
-                            var ck1 = $.trim($(this).find("#tuto_name").text().toLowerCase());
-                            var ck2 = $.trim($(this).find("#tuto_info").text().toLowerCase());
-                            var ck3 = $.trim($(this).find("#tuto_seller").text().toLowerCase());
-                            var val1 = $.trim($('input[name="tuto_name"]').val().toLowerCase());
-                            var val2 = $.trim($('input[name="tuto_info"]').val().toLowerCase());
-                            var val3 = $.trim($('select[name="tuto_seller"]').val().toLowerCase());
-                            if ((ck1 != val1 && val1 != '') || ck2.indexOf(val2) == -1 || (ck3 != val3 && val3 != '')) {
-                                $(this).hide();
-                            } else {
-                                $(this).show();
+                    success: function(data) {
+                        if (data != '01') {
+                            var data = JSON.parse(data);
+                            for (var prop in data) {
+                                $("#" + prop).html(data[prop]).show();
                             }
-                        });
-                        $('#filterbutton').prop('disabled', true);
-                    });
-                    $('.filterselect').change(function() {
-                        $('#filterbutton').prop('disabled', false);
-                    });
-                    $('.filterinput').keyup(function() {
-                        $('#filterbutton').prop('disabled', false);
-                    });
+                        } else {
+                            window.location = "logout.html";
+                        }
+                    }
+                });
 
-function buythistool(id){
-  bootbox.confirm("Are you sure?", function(result) {
-        if(result ==true){
-      $.ajax({
-     method:"GET",
-     url:"buytuto.php?id="+id+"&t=tutorials",
-     dataType:"text",
-     success:function(data){
-         if(data.match(/<button/)){
-		 $("#tuto"+id).html(data).show();
-         }else{
-            bootbox.alert('<center><img src="files/img/balance.png"><h2><b>No enough balance !</b></h2><h4>Please refill your balance <a class="btn btn-primary btn-xs"  href="addBalance.html" onclick="window.open(this.href);return false;" >Add Balance <span class="glyphicon glyphicon-plus"></span></a></h4></center>')
-         }
-     },
-   });
-       ;}
-  });
-}
+            }
+            setInterval(function() {
+                ajaxinfo()
+            }, 3000);
 
-function openitem(order){
-  $("#myModalLabel").text('Order #'+order);
-  $('#myModal').modal('show');
-  $.ajax({
+            ajaxinfo();
+
+$(document).keydown(function(event){
+    if(event.which=="17")
+        cntrlIsPressed = true;
+});
+
+$(document).keyup(function(){
+    cntrlIsPressed = false;
+});
+
+var cntrlIsPressed = false;
+
+
+function pageDiv(n,t,u,x){
+  if(cntrlIsPressed){
+    window.open(u, '_blank');
+    return false;
+  }
+        var obj = { Title: t, Url: u };
+        if ( ("/"+obj.Url) != location.pathname) {
+        	if (x != 1) {history.pushState(obj, obj.Title, obj.Url);}
+        	else{history.replaceState(obj, obj.Title, obj.Url);}
+
+    	}
+      document.title = obj.Title;
+    $("#mainDiv").html('<div id="mydiv"><img src="files/img/load2.gif" class="ajax-loader"></div>').show();
+    $.ajax({
     type:       'GET',
-    url:        'showOrder'+order+'.html',
+    url:        'divPage'+n+'.html',
     success:    function(data)
     {
-        $("#modelbody").html(data).show();
-    }});
+        $("#mainDiv").html(data).show();
+        newTableObject = document.getElementById('table');
+        //sorttable.makeSortable(newTableObject);
+       // $(".sticky-header").floatThead({top:60});
+        if(x==0){ajaxinfo();}
+      }});
+    if (typeof stopCheckBTC === 'function') { 
+    var a = stopCheckBTC();
+     }
 
 }
 
+$(window).on("popstate", function(e) {
+        location.replace(document.location);
+
+});
+
+
+$(window).on('load', function() {
+$('.dropdown').hover(function(){ $('.dropdown-toggle', this).trigger('click'); });
+   pageDiv(2,'cPanel - JeruxShop','cPanel.html',1);
+   var clipboard = new Clipboard('.copyit');
+    clipboard.on('success', function(e) {
+      setTooltip(e.trigger, 'Copied!');
+      hideTooltip(e.trigger);
+      e.clearSelection();
+   });
+
+});
+
+
+function setTooltip(btn, message) {
+  console.log("hide-1");
+  $(btn).tooltip('hide')
+    .attr('data-original-title', message)
+    .tooltip('show');
+     console.log("show");
+}
+
+function hideTooltip(btn) {
+  setTimeout(function() {$(btn).tooltip('hide'); console.log("hide-2");}, 1000);
+}
 </script>
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel"></h4>
-      </div>
-      <div class="modal-body" id="modelbody">
+
+<div id="mainDiv">
 
 
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
+
+
