a0a04a31378d626f2a5e6421ddc14266002a6151
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-19 08:55:38 +0100

    Update cPanel.php

--- buyer/cPanel.php
+++ buyer/cPanel.php
@@ -68,8 +68,8 @@ function pageDiv(n,t,u,x){
     {
         $("#mainDiv").html(data).show();
         newTableObject = document.getElementById('table');
-        sorttable.makeSortable(newTableObject);
-        $(".sticky-header").floatThead({top:60});
+        //sorttable.makeSortable(newTableObject);
+       // $(".sticky-header").floatThead({top:60});
         if(x==0){ajaxinfo();}
       }});
     if (typeof stopCheckBTC === 'function') { 
